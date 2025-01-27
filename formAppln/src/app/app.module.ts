801ddfbe081e4abd287054818d20532f9bfedfbb
subhashree ramachandran <subhashree.ramachandran@lseg.com>
2025-01-27 16:30:51 +0530

    Initial commit

---
+++ formAppln/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignupUpPageComponent } from './components/signup-up-page/signup-up-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupUpPageComponent,
+    LoginPageComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
